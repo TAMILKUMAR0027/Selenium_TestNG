d1f46b74068b96ed4d0ddceaee0819610e99adbd
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-05-05 09:25:54 +0530

    Page Object

--- Page_Object_Model/src/test/java/com/utilities/data.java
+++ Page_Object_Model/src/test/java/com/utilities/data.java
@@ -10,38 +10,46 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 
 public class data {
-  @DataProvider(name="ValidData",parallel = true)
-  public Object[][] excelDataProvider() throws IOException{
-	  Object[][] arrayObj = getExcelData("D:\\Assessment\\Page_Object_Model\\src\\test\\resources\\OrangeHrm.xlsx","Sheet1"); 
-	  return arrayObj;
-  }
-  @DataProvider(name="InvalidData",parallel = true)
-  public Object[][] excelDataProvider1()throws IOException{
-	  Object[][] arrayObj = getExcelData("D:\\Assessment\\Page_Object_Model\\src\\test\\resources\\OrangeHrm.xlsx","Sheet2"); 
-	  return arrayObj;
-  }
-  public String[][] getExcelData(String filename,String sheetName)throws IOException{
-	  String[][] data=null;
-		 try {
-			 FileInputStream fs=new FileInputStream(filename);
-			 XSSFWorkbook workbook=new XSSFWorkbook(fs);
-			 XSSFSheet sheet=workbook.getSheet(sheetName);
-			 XSSFRow row=sheet.getRow(0);
-			 int noOfRows=sheet.getPhysicalNumberOfRows();
-			 int noOfCols=row.getLastCellNum();
-			 Cell cell;
-			 data=new String[noOfRows-1][noOfCols];
-			 for(int i=1;i<noOfRows;i++) {
-				 for( int j=0;j<noOfCols;j++) {
-					 row=sheet.getRow(i);
-					 cell=row.getCell(j);
-					 data[i-1][j]=cell.getStringCellValue();
-				 }
-			 }
-		 }catch (Exception e) {
-			 System.out.println(e.getMessage());
-		 }
-		 return data;
-	 }
 
+    String filePath = "C:\\Users\\tamil\\git\\repository18\\Page_Object_Model\\src\\test\\resources\\OrangeHrm.xlsx";
+
+    @DataProvider(name = "ValidData", parallel = true)
+    public Object[][] excelDataProvider() throws IOException {
+        return getExcelData(filePath, "Sheet1");
+    }
+
+    @DataProvider(name = "InvalidData", parallel = true)
+    public Object[][] excelDataProvider1() throws IOException {
+        return getExcelData(filePath, "Sheet2");
+    }
+
+    public String[][] getExcelData(String filename, String sheetName) throws IOException {
+
+        FileInputStream fs = new FileInputStream(filename);
+        XSSFWorkbook workbook = new XSSFWorkbook(fs);
+
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+        XSSFRow row = sheet.getRow(0);
+
+        int noOfRows = sheet.getPhysicalNumberOfRows();
+        int noOfCols = row.getLastCellNum();
+
+        String[][] data = new String[noOfRows - 1][noOfCols];
+
+        for (int i = 1; i < noOfRows; i++) {
+
+            row = sheet.getRow(i);
+
+            for (int j = 0; j < noOfCols; j++) {
+
+                Cell cell = row.getCell(j);
+                data[i - 1][j] = cell.getStringCellValue();
+            }
+        }
+
+        workbook.close();
+        fs.close();
+
+        return data;
+    }
 }
